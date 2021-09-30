42bada2915db6ce3d4cc3249c054f283bf30b68e
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-30 23:17:19 +0600

    areaChart added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import MyAreaChart from './components/MyAreaChart/MyAreaChart';
 import MyLineChart from './components/MyLineChart/MyLineChart';
 
 function App() {
   return (
     <div className="App">
       <MyLineChart></MyLineChart>
+      <MyAreaChart></MyAreaChart>
     </div>
   );
 }
